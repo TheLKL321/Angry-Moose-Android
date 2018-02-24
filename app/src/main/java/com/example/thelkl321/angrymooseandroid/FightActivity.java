129e5e52dd256d1070ad67ed2fae7dc2fef52f0e
Łukasz Zarębski <lkl.zarebski@gmail.com>
2018-02-24 20:09:06 +0100

    create moose's turn and prepare template for commentary

--- app/src/main/java/com/example/thelkl321/angrymooseandroid/FightActivity.java
+++ app/src/main/java/com/example/thelkl321/angrymooseandroid/FightActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import com.example.thelkl321.angrymooseandroid.SurrenderDialogFragment.SurrenderDialogListener;
 
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FightActivity extends AppCompatActivity implements SurrenderDialogListener{
 
@@ -22,6 +23,7 @@ public class FightActivity extends AppCompatActivity implements SurrenderDialogL
     private TextView turnCounterText;
     private static int mooseHp, playerHp, turnCounter;
     private static boolean ifAttack, ifKick, ifThrow, ifDodge, ifLeap = true;
+    private static String mooseMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,38 +69,192 @@ public class FightActivity extends AppCompatActivity implements SurrenderDialogL
             playerHp += amount;
             playerHpBar.setProgress(playerHp);
         }
+    }
+
+    private void checkEndgame (){
         if (mooseHp <= 0 && playerHp <= 0) endgame("tie");
         else if (mooseHp <= 0) endgame("win");
         else if (playerHp <= 0) endgame("loss");
+    }
+
+    private static int random() {
+        return ThreadLocalRandom.current().nextInt(0, 100 + 1);
     }
 
     private void updateTurns (){
         turnCounter++;
         turnCounterText.setText(String.valueOf(turnCounter));
     }
+
+    // TODO: commentary
+    private void mooseTurn (){
+        int number = random();
+        if (number < 17) {
+            mooseMove = "lower head";
+            logEvent("The moose lowers his head");
+        }
+        else if (number < 34) {
+            mooseMove = "roar";
+            logEvent("The beast ");
+        }
+        else if (number < 51) {
+            mooseMove = "step back";
+
+        }
+        else if (number < 68) mooseMove = "eat";
+        else if (number < 85) mooseMove = "pick up";
+        else mooseMove = "turn around";
+    }
+
+    //TODO: commentary
+    private void finishTurn (String playerMove){
+        switch (mooseMove){
+            case "lower head":
+                switch (playerMove){
+                    case "throw":
+                        break;
+
+                    case "dodge":
+                        break;
+
+                    case "leap":
+                        break;
+
+                    case "kick":
+                        break;
+
+                    case "attack":
+                        break;
+                }
+                break;
+
+            case "roar":
+                switch (playerMove){
+                    case "throw":
+                        break;
+
+                    case "dodge":
+                        break;
+
+                    case "leap":
+                        break;
+
+                    case "kick":
+                        break;
+
+                    case "attack":
+                        break;
+                }
+                break;
+
+            case "step back":
+                switch (playerMove){
+                    case "throw":
+                        break;
+
+                    case "dodge":
+                        break;
+
+                    case "leap":
+                        break;
+
+                    case "kick":
+                        break;
+
+                    case "attack":
+                        break;
+                }
+                break;
+
+            case "eat":
+                switch (playerMove){
+                    case "throw":
+                        break;
+
+                    case "dodge":
+                        break;
+
+                    case "leap":
+                        break;
+
+                    case "kick":
+                        break;
+
+                    case "attack":
+                        break;
+                }
+                break;
+
+            case "pick up":
+                switch (playerMove){
+                    case "throw":
+                        break;
+
+                    case "dodge":
+                        break;
+
+                    case "leap":
+                        break;
+
+                    case "kick":
+                        break;
+
+                    case "attack":
+                        break;
+                }
+                break;
+
+            case "turn around":
+                switch (playerMove){
+                    case "throw":
+                        break;
+
+                    case "dodge":
+                        break;
+
+                    case "leap":
+                        break;
+
+                    case "kick":
+                        break;
+
+                    case "attack":
+                        break;
+                }
+                break;
+
+            default:
+                throw new NullPointerException();
+        }
+
+        checkEndgame();
+        updateTurns();
+        mooseTurn();
+    }
+
     private void logEvent (String text){
         TextView logBox = findViewById(R.id.logText);
         logBox.append("\n" + text);
     }
 
     public void throwPressed (View view){
-        changeHealth("moose",-3);
+        finishTurn("throw");
     }
 
     public void dodgePressed (View view){
-        logEvent("lolololololol");
+        finishTurn("dodge");
     }
 
     public void leapPressed (View view){
-        changeHealth("player", -3);
+        finishTurn("leap");
     }
 
     public void kickPressed (View view){
-
+        finishTurn("kick");
     }
 
     public void attackPressed (View view){
-
+        finishTurn("attack");
     }
 
     public void logPressed (View view){
