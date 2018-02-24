b2bff047fc9f73725752ad7f1853087d035ccd9b
Łukasz Zarębski <lkl.zarebski@gmail.com>
2018-02-24 19:41:47 +0100

    make a turn counter

--- app/src/main/java/com/example/thelkl321/angrymooseandroid/EndgameActivity.java
+++ app/src/main/java/com/example/thelkl321/angrymooseandroid/EndgameActivity.java
@@ -21,22 +21,31 @@ public class EndgameActivity extends AppCompatActivity {
         String outcome = intent.getStringExtra(FightActivity.OUTCOME_KEY);
         mooseHp = intent.getIntExtra(PlayActivity.MOOSE_KEY, 0);
         playerHp = intent.getIntExtra(PlayActivity.PLAYER_KEY, 0);
+        int turnCounter = intent.getIntExtra(FightActivity.TURN_COUNTER_KEY, 0);
 
         // Set outcome
         TextView text = findViewById(R.id.outcomeText);
+        TextView score = findViewById(R.id.turnText);
         switch (outcome){
             case "tie":
                 text.setText("IT'S A\nTIE");
+                if (turnCounter == 1) score.setText("You survived one turn");
+                else score.setText("You survived " + turnCounter + " turns");
                 break;
 
             case "win":
                 text.setText("YOU\nWON");
+                if (turnCounter == 1) score.setText("You won in one turn!"); //that's not possible
+                else score.setText("You won in " + turnCounter + " turns");
                 break;
 
             case "loss":
                 text.setText("YOU\nLOST");
+                if (turnCounter == 1) score.setText("You survived one turn");
+                else score.setText("You survived " + turnCounter + " turns");
                 break;
         }
+
     }
 
     public void backPressed (View view){
@@ -48,6 +57,7 @@ public class EndgameActivity extends AppCompatActivity {
         intent.putExtra(PlayActivity.MOOSE_KEY, mooseHp);
         intent.putExtra(PlayActivity.PLAYER_KEY, playerHp);
         startActivity(intent);
+        finish();
     }
 
     public void sharePressed (View view){
--- app/src/main/java/com/example/thelkl321/angrymooseandroid/FightActivity.java
+++ app/src/main/java/com/example/thelkl321/angrymooseandroid/FightActivity.java
@@ -1,5 +1,6 @@
 package com.example.thelkl321.angrymooseandroid;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,14 +9,19 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.thelkl321.angrymooseandroid.SurrenderDialogFragment.SurrenderDialogListener;
+
 import java.util.Objects;
 
-public class FightActivity extends AppCompatActivity{
+public class FightActivity extends AppCompatActivity implements SurrenderDialogListener{
+
+    public static final String TURN_COUNTER_KEY = "turn";
+    public static final String OUTCOME_KEY = "outcome";
 
     private ProgressBar mooseHpBar, playerHpBar;
-    private static int mooseHp, playerHp;
+    private TextView turnCounterText;
+    private static int mooseHp, playerHp, turnCounter;
     private static boolean ifAttack, ifKick, ifThrow, ifDodge, ifLeap = true;
-    public static final String OUTCOME_KEY = "outcome";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,11 @@ public class FightActivity extends AppCompatActivity{
         mooseHpBar.setMax(mooseHp);
         playerHpBar.setMax(playerHp);
 
+        // Assign turn counter
+        turnCounter = 1;
+        turnCounterText = findViewById(R.id.turnCounterText);
+        turnCounterText.setText(String.valueOf(turnCounter));
+
         // Create scrollable log
         ((TextView) findViewById(R.id.logText)).setMovementMethod(new ScrollingMovementMethod());
     }
@@ -42,6 +53,7 @@ public class FightActivity extends AppCompatActivity{
         intent.putExtra(OUTCOME_KEY, outcome);
         intent.putExtra(PlayActivity.MOOSE_KEY, mooseHpBar.getMax());
         intent.putExtra(PlayActivity.PLAYER_KEY, playerHpBar.getMax());
+        intent.putExtra(TURN_COUNTER_KEY, turnCounter);
         startActivity(intent);
         finish();
     }
@@ -60,6 +72,10 @@ public class FightActivity extends AppCompatActivity{
         else if (playerHp <= 0) endgame("loss");
     }
 
+    private void updateTurns (){
+        turnCounter++;
+        turnCounterText.setText(String.valueOf(turnCounter));
+    }
     private void logEvent (String text){
         TextView logBox = findViewById(R.id.logText);
         logBox.append("\n" + text);
@@ -89,5 +105,19 @@ public class FightActivity extends AppCompatActivity{
         // TODO: useless button
     }
 
-    //TODO: handle back button
+    @Override
+    public void onBackPressed (){
+        DialogFragment dialog = new SurrenderDialogFragment();
+        dialog.show(getFragmentManager(), "surrender");
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        endgame("loss");
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        //Do nothing
+    }
 }
